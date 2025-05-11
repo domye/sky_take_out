043fdfc8b26fb88eff000e8d86d9cb55ea39dad5
Domye <a1523610551@163.com>
2025-05-11 21:36:20 +0800

    增加小程序登录接口

---
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.sky.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import com.sky.entity.User;
+
+@Mapper
+public interface UserMapper {
+    @Select("SELECT * FROM user WHERE openid = #{id}")
+    public User getUserById(String openid);
+
+    void insert(User user);
+}
