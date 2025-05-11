49488d28754966ed6b2415c3893d727d85a86cb3
Domye <a1523610551@163.com>
2025-05-11 16:34:37 +0800

    增加测试类

---
+++ sky-server/src/test/java/com/sky/test/SpringDataRedisTest.java
@@ -0,0 +1,40 @@
+package com.sky.test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.core.ZSetOperations;
+
+//@SpringBootTest
+public class SpringDataRedisTest {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void testRedis() {
+        System.out.println(redisTemplate);
+        ValueOperations valueOperations = redisTemplate.opsForValue();
+        HashOperations hashOperations = redisTemplate.opsForHash();
+        ListOperations listOperations = redisTemplate.opsForList();
+        SetOperations setOperations = redisTemplate.opsForSet();
+        ZSetOperations zSetOperations = redisTemplate.opsForZSet();
+    }
+
+    @Test
+    public void testString() {
+        redisTemplate.opsForValue().set("name", "sky");
+        String name = (String) redisTemplate.opsForValue().get("name");
+        System.out.println(name);
+        redisTemplate.opsForValue().set("code", "1234", 2, TimeUnit.MINUTES);
+        redisTemplate.opsForValue().setIfAbsent("lock", "1");
+        redisTemplate.opsForValue().setIfAbsent("lock", "2");
+    }
+}
